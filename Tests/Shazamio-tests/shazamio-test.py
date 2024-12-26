1400aadcc4475c32284a8d7f12e3704f6129cdfa
Sotiris Sotiriou <sotirios.sot@outlook.com>
2024-12-26 17:33:54 +0200

    `Removed ShazamAPI-tests and updated Shazamio-tests to use command line arguments and print song metadata`

--- Tests/Shazamio-tests/shazamio-test.py
+++ Tests/Shazamio-tests/shazamio-test.py
@@ -1,5 +1,20 @@
 import asyncio
 from shazamio import Shazam
+import sys
+
+# sys.argv contains the script name and arguments
+if len(sys.argv) != 2:
+    print("Usage: python shazamio-test.py <mp3_file_path>")
+    exit(1)
+else:
+    mp3_file = sys.argv[1]
+
+# Create a function to fetch the text by title
+def get_text_by_title(data, title):
+    for item in data:
+        if item['title'] == title:
+            return item['text']
+    return None  # Return None if the title is not found
 
 async def recognize_song(file_path):
     shazam = Shazam()
@@ -13,15 +28,25 @@ async def recognize_song(file_path):
     
     # Print out the song's information
     if out:
-        # print(f"Song recognized: {out['track']['title']} by {out['track']['subtitle']}")
-        # print(f"Album: {out['track']['sections'][0]['metadata'][0]['text']}")
-        # print(f"Shazam link: {out['track']['url']}")
-        print(out)
+        print(f"Song: {out['track']['sections'][0]['metapages'][1]['caption']}")
+        return out
     else:
         print("Song could not be recognized.")
-
-# File path to the MP3 you want to recognize
-mp3_file = "a.mp3"
+        return None
 
 # Run the async function
-asyncio.run(recognize_song(mp3_file))
+print("Song recognizer result: ")
+shazam_output = asyncio.run(recognize_song(mp3_file))
+metadata = shazam_output['track']['sections'][0]['metadata']
+
+singer = shazam_output['track']['sections'][0]['metapages'][0]['caption']
+title = shazam_output['track']['sections'][0]['metapages'][1]['caption']
+album = get_text_by_title(metadata, "Album")
+released_year = get_text_by_title(metadata, "Released")
+
+# print(shazam_output)
+print("\n\nInfo retrieved: ")
+print("Singer: ", singer)
+print("Title: ", title)
+print("Album: ", album)
+print("Release Year: ", released_year)
